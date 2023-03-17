3eebc036e032bad9238920555e3b2270487e4a4c
Maycon Vieira <maycon-big@hotmail.com>
2023-03-16 21:36:37 -0300

    (feat): making the first connection with the back

--- agendeJaFront/src/components/Header/Header.js
+++ agendeJaFront/src/components/Header/Header.js
@@ -5,7 +5,6 @@ import "./Header.scss";
 export default function Header() {
   const [isExpanded, setIsExpanded] = useState(false);
 
-
   return (
     <nav className="navigation">
       <a href="/" className="brand-name">
@@ -18,30 +17,34 @@ export default function Header() {
         }}
       >
         <div className={isExpanded ? "container change" : "container"}>
-          <div className="bar1"/>
-          <div className="bar2"/>
-          <div className="bar3"/>
+          <div className="bar1" />
+          <div className="bar2" />
+          <div className="bar3" />
         </div>
       </button>
       <div
-        className={
-          isExpanded ? "navigation-menu expanded" : "navigation-menu"
-        }
+        className={isExpanded ? "navigation-menu expanded" : "navigation-menu"}
       >
         <ul>
-          <li onClick={() => {
-          setIsExpanded(!isExpanded);
-        }}>
+          <li
+            onClick={() => {
+              setIsExpanded(!isExpanded);
+            }}
+          >
             <Link to="/">Pagina inicial</Link>
           </li>
-          <li onClick={() => {
-          setIsExpanded(!isExpanded);
-        }}>
+          <li
+            onClick={() => {
+              setIsExpanded(!isExpanded);
+            }}
+          >
             <Link to="/login">Logar</Link>
           </li>
-          <li onClick={() => {
-          setIsExpanded(!isExpanded);
-        }}>
+          <li
+            onClick={() => {
+              setIsExpanded(!isExpanded);
+            }}
+          >
             <Link to="/cadastrar">Cadastrar</Link>
           </li>
         </ul>
--- agendeJaFront/src/components/Login/Login.js
+++ agendeJaFront/src/components/Login/Login.js
@@ -1,34 +1,50 @@
-import React, { useState } from 'react';
-import './login.scss'
+import React, { useState } from "react";
+import "./login.scss";
+import axios from "axios";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  }
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Aqui você pode adicionar sua lógica de autenticação
-  }
+
+    axios
+      .post("http://localhost:8080/agenda/login", {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response.data);
+        console.log("Deubom" + response.data);
+        // atualizar o estado do componente com a mensagem de sucesso
+        alert("Logado com sucesso");
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        // atualizar o estado do componente com a mensagem de erro
+      });
+  };
 
   return (
-    <div className='containerDad'>
+    <div className="containerDad">
       <form onSubmit={handleSubmit}>
         <h1>Login</h1>
         <label>
           Email:
-          <input type="email" value={email} onChange={handleEmailChange} />
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </label>
         <label>
           Password:
-          <input type="password" value={password} onChange={handlePasswordChange} />
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </label>
         <div>
           <button type="submit">Login</button>
@@ -36,6 +52,6 @@ const Login = () => {
       </form>
     </div>
   );
-}
+};
 
 export default Login;
