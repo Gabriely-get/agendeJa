5bb3bbda07e454b55db49b18647a95ecf1fa9208
Gabriely-get <gabrielysantos.informatica@gmail.com>
2023-09-02 12:56:52 -0300

    perf: if-else. Install ansible if not installed

--- setup.sh
+++ setup.sh
@@ -4,7 +4,13 @@ echo "---------- update yum ----------"
 sudo yum update
 
 echo "---------- install ansible----------"
-sudo amazon-linux-extras install ansible2 -y
+
+if ! command -v ansible &> /dev/null; then
+    echo "Ansible is not installed. Installing..."
+    sudo amazon-linux-extras install ansible2 -y
+else
+    echo "Ansible is already installed."
+fi
 
 echo "---------- run playbook ----------"
 cd playbooks
